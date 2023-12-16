0b090f805a7b136d86ed9e981767a1a0dabc397a
Sarjyant Shrestha <sarjyant@gmail.com>
2023-12-16 17:51:58 +0545

    updated nvim

--- nvim/lua/rapzlua/lazy.lua
+++ nvim/lua/rapzlua/lazy.lua
@@ -109,12 +109,6 @@ require('lazy').setup({
     },
   },
 
-  { -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    main = "ibl",
-    opts = {},
-  },
-
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 
