eefd039f9ae786bff3e1c1def41c3c19c7359c7c
Sarjyant Shrestha <sarjyant@gmail.com>
2023-12-16 17:28:32 +0545

    nvim:

---
+++ nvim/lua/rapzlua/init.lua
@@ -0,0 +1,8 @@
+require("rapzlua.lazy")
+require("rapzlua.set")
+require("rapzlua.telescope")
+require("rapzlua.treesitter")
+require("rapzlua.lsp-nvim-cmp")
+require("rapzlua.colors")
+require("rapzlua.macros")
+require("rapzlua.nvim-tree")
