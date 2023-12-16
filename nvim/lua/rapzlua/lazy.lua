b749de9c71949e1a577ad4390d65c48073d9b0ff
Sarjyant Shrestha <sarjyant@gmail.com>
2023-12-16 18:24:26 +0545

    tmux nav

--- nvim/lua/rapzlua/lazy.lua
+++ nvim/lua/rapzlua/lazy.lua
@@ -34,10 +34,15 @@ require('lazy').setup({
 
   --nvim-tree
   'nvim-tree/nvim-tree.lua',
+
   --devicons
   'nvim-tree/nvim-web-devicons',
+
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
+
+  --tmux navigator
+  'christoomey/vim-tmux-navigator',
 
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
