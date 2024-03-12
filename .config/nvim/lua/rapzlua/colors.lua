4dadc72683d2c982575de18d409d8134240a3df4
rapzy <sarjyant@gmail.com>
2024-03-12 09:53:50 +0545

    removed all nvim old config

--- .config/nvim/lua/rapzlua/colors.lua
+++
@@ -1,9 +0,0 @@
-function ColorMyPencils(color)
-  color = color or "onedark"
-  vim.cmd.colorscheme("rose-pine")
-  --[[Transparent Terminal]]
-  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-end
-
-ColorMyPencils()
